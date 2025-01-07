3cded0f0ccdac03e573f2ace6a64e1950f92fdfe
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-07 22:06:23 +0200

    added database
    added to  models first table event and the fileds of it

--- eventmanagementsystem/eventmanagementsystem/asgi.py
+++ eventmanagementsystem/eventmanagementsystem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for eventmangementsystem project.
+ASGI config for eventmanagementsystem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmangementsystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmanagementsystem.settings')
 
 application = get_asgi_application()
