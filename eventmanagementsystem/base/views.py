c5b0d583b4031bb20b264f4d5c2e84437322b770
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-07 22:37:25 +0200

    finished homepage
    homepage display all events with view function to display and home.html

--- eventmanagementsystem/base/views.py
+++ eventmanagementsystem/base/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
 # Create your views here.
+from .models import Event
+def displayEvents(request):
+    events = Event.objects.all()
+    return render(request, 'base/home.html', {'events': events})
+
